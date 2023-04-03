91c09a8024b4628bf4b1dcbf2d2c2b22f1433e12
andrew-wilkes <github@urgentclick.com>
2023-04-03 22:14:54 +0900

    Add test code

--- big_cube.gd
+++ big_cube.gd
@@ -18,28 +18,40 @@ func _ready():
 				cube.get_child(4).visible = x == -1
 				cube.get_child(1).visible = z == 1
 				cube.get_child(3).visible = z == -1
-	var group_idx = 5
+	
+	# Test
+	var group_idx = 0
 	var group = get_group(group_idx)
 	reparent_to_pivot(group)
 	rotate_group(group_idx, group)
+	for cube in get_children():
+		print(cube.position)
+	#group_idx = 5
+	#group = get_group(group_idx)
+	#reparent_to_pivot(group)
+	#rotate_group(group_idx, group)
 
 
 func rotate_group(idx, group, dir = 1):
+	var rot_angle = PI / 2 * dir
 	match idx:
 		0,1:
-			group[4].rotate_x(PI / 4 * dir)
+			group[4].rotate_x(rot_angle)
 		2,3:
-			group[4].rotate_y(PI / 4 * dir)
+			group[4].rotate_y(rot_angle)
 		4,5:
-			group[4].rotate_z(PI / 4 * dir)
+			group[4].rotate_z(rot_angle)
 
 
 func get_group(idx):
 	var pivot_pos = [Vector3(-1,0,0),Vector3(1,0,0),Vector3(0,-1,0),Vector3(0,1,0),Vector3(0,0,-1),Vector3(0,0,1)]
 	var group = []
 	for cube in get_children():
+		cube.reparent(self)
+		print(cube.position)
 		if (cube.position.dot(pivot_pos[idx]) > 0.5):
 			group.append(cube)
+	print("---")
 	return group
 
 
