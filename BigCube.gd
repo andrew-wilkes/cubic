272545e9b025d301f3d0ec00039ff491610c9896
andrew-wilkes <github@urgentclick.com>
2023-04-03 20:32:43 +0900

    Use materials instead of shader

--- BigCube.gd
+++ BigCube.gd
@@ -1,29 +1,17 @@
 extends Node3D
 
-var shader = preload("res://assets/cube.gdshader")
 var piece = preload("res://cube.tscn")
 
-@export_color_no_alpha var col1: Color
-@export_color_no_alpha var col2: Color
-@export_color_no_alpha var col3: Color
-@export_color_no_alpha var col4: Color
-@export_color_no_alpha var col5: Color
-@export_color_no_alpha var col6: Color
-
 func _ready():
-	var colors = [col1, col2, col3, col4, col5, col6]
-	var instance: Node3D = piece.instantiate()
-	# Apply colors to faces
-	for idx in 6:
-		instance.get_child(idx).material.set_shader_parameter("color", colors[idx])
 	# Generate the cube
+	var instance: Node3D = piece.instantiate()
 	for x in [-1, 0, 1]:
 		for y in [-1, 0, 1]:
 			for z in [-1, 0, 1]:
 				var cube = instance.duplicate()
 				cube.position = Vector3(x, y, z)
 				add_child(cube)
-				# Hide unseen faces
+				# Set face visibility
 				cube.get_child(0).visible = y == 1
 				cube.get_child(5).visible = y == -1
 				cube.get_child(2).visible = x == 1
