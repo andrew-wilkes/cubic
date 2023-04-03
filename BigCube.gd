517a2697c2ea9d6cc653f05e4553037e2c9d4b8a
andrew-wilkes <github@urgentclick.com>
2023-04-03 17:50:11 +0900

    Add code to store groups of cubes

--- BigCube.gd
+++ BigCube.gd
@@ -17,13 +17,20 @@ func _ready():
 	# Apply colors to faces
 	for idx in 6:
 		instance.get_child(idx).material.set_shader_parameter("color", colors[idx])
+	# Init the cubes array to store groups of cubes
+	cubes.resize(9)
+	for idx in 9:
+		cubes[idx] = []
 	# Generate the cube
 	for x in [-1, 0, 1]:
 		for y in [-1, 0, 1]:
 			for z in [-1, 0, 1]:
 				var cube = instance.duplicate()
 				cube.position = Vector3(x, y, z)
-				cubes.append(cube)
+				# Add cube to group
+				cubes[x + 1].append(cube)
+				cubes[y + 4].append(cube)
+				cubes[z + 7].append(cube)
 				add_child(cube)
 				# Hide unseen faces
 				cube.get_child(0).visible = y == 1
@@ -32,3 +39,4 @@ func _ready():
 				cube.get_child(4).visible = x == -1
 				cube.get_child(1).visible = z == 1
 				cube.get_child(3).visible = z == -1
+	print(cubes)
