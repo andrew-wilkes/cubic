5872c164c02d3b4e94e013e85aa04f7bfc39b75e
andrew-wilkes <github@urgentclick.com>
2023-04-06 12:51:40 +0900

    Simplify solved method

--- big_cube.gd
+++ big_cube.gd
@@ -69,14 +69,7 @@ func _process(delta):
 
 
 func solved():
-	return get_cube_state_signature() < 0.1
-
-
-func get_cube_state_signature():
-	var sum = Vector3.ZERO
-	for cube in get_children():
-		sum = cube.rotation + sum
-	return sum.length_squared()
+	return face_rotations.count(0) == 6
 
 
 func rotate_face(idx, dir = 1):
--- main.gd
+++ main.gd
@@ -5,6 +5,7 @@ var face = 0
 func _on_button_pressed():
 	face = (face + 1) % 6
 	$BigCube.rotate_face(face)
+	print($BigCube.solved())
 
 
 func _process(_delta):
