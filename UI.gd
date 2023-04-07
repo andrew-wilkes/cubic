39f031cad852f175438c141481838f986154ced6
andrew-wilkes <github@urgentclick.com>
2023-04-07 14:16:22 +0900

    Add UI buttons

---
+++ UI.gd
@@ -0,0 +1,14 @@
+extends Control
+
+signal button_pressed(id, shift)
+func _ready():
+	var nodes = $Grid.get_children()
+	nodes.append_array(self.get_children())
+	for node in nodes:
+		if node is Button:
+			node.pressed.connect(_on_button_pressed.bind(node.name))
+
+func _on_button_pressed(bname: String):
+	var shift = Input.is_key_pressed(KEY_SHIFT) \
+		or Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
+	emit_signal("button_pressed", bname, shift)
--- main.gd
+++ main.gd
@@ -1,13 +1,2 @@
 extends Node3D
 
-var face = 0
-
-func _on_button_pressed():
-	face = (face + 1) % 6
-	$BigCube.rotate_face(face)
-	print($BigCube.solved())
-
-
-func _process(_delta):
-	var vec = $OrbitingCamera/XAxis/YAxis/Camera.global_position.normalized()
-	$C/UI/Label.text = str($BigCube.get_face(vec))
