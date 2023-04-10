80e0b0fe6ded2ba0c9eaed2f65cb79729cfb1bae
andrew-wilkes <github@urgentclick.com>
2023-04-10 10:11:17 +0900

    Move tile between selected tiles

--- scenes/color_map.gd
+++ scenes/color_map.gd
@@ -2,11 +2,15 @@ extends Control
 
 const COLORS = [Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.WHITE, Color.ORANGE]
 
-# See coordinates diagram for index relationships
+# See coordinates diagram for index relationships in media folder
+# The _FACE_MAP constants are indexing the faces
+# Copies of the FACE_MAP constants are indexing the changing colors on the faces of the pieces
 const CORNER_FACE_MAP = [[0,1,5],[0,5,3],[0,2,1],[0,3,2],[1,4,5],[1,2,4],[2,3,4],[3,5,4]]
 const EDGE_FACE_MAP = [[0,5],[0,1],[0,3],[0,2],[1,5],[1,2],[2,3],[3,5],[1,4],[2,4],[3,4],[4,5]]
+# FACE_ maps contain a list of edge/corner indices of parts on a face
 const FACE_CORNER_MAP = [[0,1,2,3],[0,2,4,5],[2,3,5,6],[3,1,6,7],[5,6,4,7],[4,7,0,1]]
 const FACE_EDGE_MAP = [[0,1,2,3],[1,4,5,8],[3,5,6,9],[2,6,7,10],[9,8,10,11],[11,4,7,0]]
+# This indexes the positions of faces under the Grid node
 const GRID_FACE_MAP = [1,3,4,5,7,10]
 
 var source_tile = null
@@ -67,9 +71,10 @@ func try_move(from_tile, to_tile):
 		if edge1 == edge2:
 			# swap colors
 			edges[edge1].reverse()
-			set_edge_color(edge1, 0)
-			set_edge_color(edge1, 1)
+			for face_idx in 2:
+				set_edge_color(edge1, face_idx)
 		else:
+			# Want from.face tile to move to to.face tile for a natural feel
 			if EDGE_FACE_MAP[edge1].find(from.face) != EDGE_FACE_MAP[edge2].find(to.face):
 				# swap colors
 				edges[edge1].reverse()
@@ -77,43 +82,47 @@ func try_move(from_tile, to_tile):
 			var edge1_colors = edges[edge1]
 			edges[edge1] = edges[edge2]
 			edges[edge2] = edge1_colors
-			set_edge_color(edge1, 0)
-			set_edge_color(edge1, 1)
-			set_edge_color(edge2, 0)
-			set_edge_color(edge2, 1)
+			for face_idx in 2:
+				set_edge_color(edge1, face_idx)
+				set_edge_color(edge2, face_idx)
 	if is_corner(from.tile) and is_corner(to.tile):
 		var corner1 = get_corner(from.face, from.tile)
 		var corner2 = get_corner(to.face, to.tile)
-		var rot_step = wrapi(CORNER_FACE_MAP[corner1].find(from.face) - CORNER_FACE_MAP[corner1].find(to.face), -1, 2)
-		# rotate colors
-		var rotated_values = [corners[corner1][rot_step], corners[corner1][rot_step + 1], corners[corner1][wrapi(rot_step + 2, 0, 3)]]
 		if corner1 == corner2:
-			corners[corner1] = rotated_values
+			# rotate colors
+			corners[corner1] = get_rotated_faces(corner1, from.face, corner1, to.face)
 			for face_idx in 3:
 				set_corner_color(corner1, face_idx)
 		else:
 			# swap corners
+			var rotated_corner1 = get_rotated_faces(corner1, from.face, corner2, to.face)
 			corners[corner1] = corners[corner2]
-			corners[corner2] = rotated_values
+			corners[corner2] = rotated_corner1
 			for face_idx in 3:
 				set_corner_color(corner1, face_idx)
 				set_corner_color(corner2, face_idx)
 
 
+func get_rotated_faces(corner1, from_idx, corner2, to_idx):
+	var rot_step = wrapi(CORNER_FACE_MAP[corner1].find(from_idx) - CORNER_FACE_MAP[corner2].find(to_idx), -1, 2)
+	return [corners[corner1][rot_step], corners[corner1][rot_step + 1], corners[corner1][wrapi(rot_step + 2, 0, 3)]]
+
+
 func is_edge(n):
 	return n % 2 == 1 # Odd number
+
 
 func is_corner(n):
 	return n % 2 == 0 # Even number
 
-func is_same_piece(from, to):
-	pass
 
 func get_edge(face, n):
 	return FACE_EDGE_MAP[face][n / 2]
 
+
 func get_corner(face, n):
 	return FACE_CORNER_MAP[face][(n + 1) / 3]
+
 
 func set_edge_color(edge_idx, face_idx):
 	# Get the face of the grid
@@ -123,12 +132,13 @@ func set_edge_color(edge_idx, face_idx):
 	var color = edges[edge_idx][face_idx]
 	set_tile_color(face, tile, color)
 
+
 func set_corner_color(corner_idx, face_idx):
 	var face = CORNER_FACE_MAP[corner_idx][face_idx]
 	var tile = FACE_CORNER_MAP[face].find(corner_idx) * 3 / 2 * 2
-	print(tile)
 	var color = corners[corner_idx][face_idx]
 	set_tile_color(face, tile, color)
+
 
 func set_tile_color(face_idx, tile_idx, color_idx):
 	$Grid.get_child(GRID_FACE_MAP[face_idx]).get_child(tile_idx).color = COLORS[color_idx]
