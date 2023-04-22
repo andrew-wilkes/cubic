1e9506d9635b293c565b329db364a1484413cc0f
andrew-wilkes <github@urgentclick.com>
2023-04-22 11:07:27 +0900

    Use a viewport for the cube

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,35 +1,32 @@
-extends Node3D
+extends Control
 
 const FACE_BUTTONS = ["U", "L", "F", "R", "D", "B"]
 
+var bc
+
 func _ready():
 	$C/UI.button_pressed.connect(_on_button_pressed)
+	bc = $C/SVP/VP/BigCube
 
 
 func _on_button_pressed(bname, shift, ctrl):
 	if FACE_BUTTONS.has(bname):
 		var button_idx = FACE_BUTTONS.find(bname)
 		if ctrl:
-			$BigCube/Pivot.rotate_to_face(button_idx)
+			bc.get_node("Pivot").rotate_to_face(button_idx)
 		else:
 			var direction = -1 if shift else 1
-			$BigCube.rotate_face(button_idx, direction, $BigCube/Pivot.transform.basis)
+			bc.rotate_face(button_idx, direction, bc.get_node("Pivot").transform.basis)
 	match bname:
 		"Reset":
-			$BigCube.reset()
+			bc.reset()
 		"Scramble":
 			for n in randi_range(10, 15):
-				$BigCube.rotate_face_immediate(randi() % 6, 1 if randf() > 0.5 else -1)
+				bc.rotate_face_immediate(randi() % 6, 1 if randf() > 0.5 else -1)
 		"Solve":
 			pass
 		"CopyCube":
-			$C/ColorMap.set_edge_colors($BigCube.get_edge_colors())
-			$C/ColorMap.set_corner_colors($BigCube.get_corner_colors())
+			$C/ColorMap.set_edge_colors(bc.get_edge_colors())
+			$C/ColorMap.set_corner_colors(bc.get_corner_colors())
 		"CopyMap":
-			$BigCube.apply_map($C/ColorMap.get_data())
-
-
-func get_rounded_rotation_value(angle):
-	# Convert -PI .. PI to 0/1/2/3
-	# The offset aligns the value with the correct face index
-	return int(round(angle / PI * 2) + 4) % 4
+			bc.apply_map($C/ColorMap.get_data())
