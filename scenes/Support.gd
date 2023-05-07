3dc907ed4436d8ffe4dbc2c19a8e25a13e6cbbf3
andrew-wilkes <github@urgentclick.com>
2023-05-07 14:30:20 +0900

    Add coffee button

--- scenes/Support.gd
+++ scenes/Support.gd
@@ -1,4 +1,17 @@
 extends PopupPanel
 
+var button: Button
+
+func _ready():
+	button = get_parent().find_child("Coffee")
+	if button:
+		button.connect("pressed", show_popup)
+
+
+func show_popup():
+	button.release_focus()
+	popup_centered()
+
+
 func _on_text_meta_clicked(_meta):
 	var _e = OS.shell_open("https://buymeacoffee.com/gdscriptdude")
